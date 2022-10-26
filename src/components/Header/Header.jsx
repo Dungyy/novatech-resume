e0afdfc035bdea29b2c663dd7c07adf0947b2ab0
Dungyy <erickmunoz13@gmail.com>
2022-10-26 17:34:36 -0500

    Set Up Env, CSS and jsx Components || Finshed Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+export default function Header() {
+  return (
+    <div className='Header-Container'>
+        <h1 className='Header-Name'> Erick Munoz</h1>
+        <h3 className='Title'>Software Engineer</h3>
+        <p className='Desc'>A software engineer with intensive education in program development. In arranging and making imaginative computer program
+        arrangements to improve commerce efficiency with a team environment. Exceedingly experienced in all angles of the program
+        advancement life cycles from end-to-end experience, from concept through development and delivery. Reliably recognized as a hands-on
+        and competent leader.</p>
+    </div>
+  )
+}
